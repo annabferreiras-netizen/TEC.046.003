bb62e27418c94049ed0041f29402d73a9fff0449
Anna Beatriz ferreira dos Santos <annabferreira.s@gmail.com>
2026-03-10 12:15:07 -0300

    atividade_funcoes

---
+++ Atividade-2/exemplo_funcoes.js
@@ -0,0 +1,17 @@
+// 1. function EXPRESSION - Valor Base
+const calcularValorBase = function (horas, precoHora) 
+{
+    return  horas * precoHora;
+}
+// 2. Arrow Function - Adicionar Taxa fixa
+const adicionarTaxa = (valor) => valor + 2.00;
+// 3. Function Declaration - Exibir Recibo
+function emitirRecibo(totalFinal)
+{
+    console.log("--- ESTACIONAMENTO --- ")
+    console.log("Total a pagar: R$ " + totalFinal.toFixed(2))
+}
+// 4. Execução
+let valorBruto = calcularValorBase(3, 10);
+let valorComTaxa = adicionarTaxa(valorBruto);
+emitirRecibo(valorComTaxa);
