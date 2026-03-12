12acc842ba8bb2f2b758dfef8da93ec883c1f6f9
Marcos Paulo Alves Borges <tenn77wolf@gmail.com>
2026-03-12 12:03:20 -0300

    Create lista_compras.js

---
+++ Atividade-3/lista_compras.js
@@ -0,0 +1,16 @@
+let lista_compras = ["aroz", "feijão", "Açúcar", "Salmão"]
+lista_compras.push("Macarrão");
+lista_compras.push("Carne");
+lista_compras.push("Frango");
+lista_compras.forEach(item =>
+     {
+    console.log(item);
+});
+
+let cesta_feutas = ["Maça", "Manga", "Uva", "Banana"]
+cesta_feutas.push("Pera");
+cesta_feutas.push("Goiaba");
+cesta_feutas.forEach(item =>
+     {
+    console.log(item);
+});
